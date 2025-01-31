f80a1480d60903091f74805d756ee59f6db46b8a
issac <issacneedscoffee@gmail.com>
2025-02-01 01:30:31 +0530

    final exercise of module_20 is completed

---
+++ module_20/question_5/main.js
@@ -0,0 +1,15 @@
+// use the while loop to print numbers from 1 to 25 in ascending and descending order.
+
+let ascendingNumber = 1;
+
+while (ascendingNumber<=25) {
+    console.log(ascendingNumber);
+    ascendingNumber++;
+}
+
+let descendingnumber = 25;
+
+while (descendingnumber>=1) {
+    console.log(descendingnumber);
+    descendingnumber--;
+}
